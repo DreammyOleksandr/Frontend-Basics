55bf175ca859e4d34ba1e7935121a5a53848a6ff
Bondarenko Oleksandr <albom2004q@gmail.com>
2024-10-08 13:20:05 +0300

    upd: Updating validation script

--- Lab 5/script.js
+++ Lab 5/script.js
@@ -4,17 +4,31 @@ validate = () => {
 
   for (let element of inputs) {
     if (element.getAttribute('regex')) {
-      inputsRegex.set(element.id, { data: element.value, regex: element.getAttribute('regex') })
+      inputsRegex.set(element.id, {
+        data: element.value,
+        regex: element.getAttribute('regex'),
+        example: element.getAttribute('example'),
+      })
     }
   }
   console.log(inputsRegex)
   inputsRegex.forEach((value, key) => {
     const element = document.getElementById(key)
-    if (!value.data.match(value.regex) && !element.classList.contains('input-danger'))
+    if (!value.data.match(value.regex) && !element.classList.contains('input-danger')) {
       element.classList.toggle('input-danger')
-    
-    if (value.data.match(value.regex) && !element.classList.contains('input-success'))
+      element.classList.remove('input-success')
+
+      const newElement = document.createElement('div')
+      newElement.textContent = `Приклад правильних даних: ${value.example}`
+      newElement.classList.toggle('text-danger')
+      element.parentNode.insertBefore(newElement, element.nextSibling)
+    }
+
+    if (value.data.match(value.regex) && !element.classList.contains('input-success')) {
       element.classList.toggle('input-success')
+      element.classList.remove('input-danger')
+      element.parentNode.removeChild(element.nextSibling)
+    }
   })
 }
 
