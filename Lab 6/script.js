042639c2db12477721854bcc34389b0845437725
Bondarenko Oleksandr <albom2004q@gmail.com>
2024-10-16 22:38:36 +0300

    upd: script refactoring

--- Lab 6/script.js
+++ Lab 6/script.js
@@ -3,7 +3,7 @@ isActivatedFetch = false
 fetchData = async (context) => {
   context.disabled = true
   if (isActivatedFetch) {
-    let warning = document.createElement('div')
+    const warning = document.createElement('div')
     warning.textContent = "You've already fetched the data. Refresh the page and try again"
     warning.classList.add('flex-container-center', 'danger-strip')
     document.body.appendChild(warning)
@@ -12,19 +12,20 @@ fetchData = async (context) => {
       await fetch('https://randomuser.me/api')
         .then((res) => res.json())
         .then((data) => {
-          let elements = []
+          const elements = []
 
           document
             .getElementById('cards')
-            .insertAdjacentHTML('beforeend', `<div id="card${i}" class="card"></div>`)
+            .insertAdjacentHTML(
+              'beforeend',
+              `<div id="card${i}" class="card flex-container-center-card-content"></div>`
+            )
 
-          const image = `<img class="margin" src="${data.results[0].picture.large}" alt="Ooops..."></img>`
-          const city = `<div class="margin">City: ${data.results[0].location.city}</div>`
-          const country = `<div class="margin">Country: ${data.results[0].location.country}</div>`
-          const name = `<div class="margin">Full Name: ${data.results[0].name.first} ${data.results[0].name.last}</div>`
-          const postcode = `<div class="margin">Postcode: ${data.results[0].location.postcode}</div>`
-
-          console.log(data.results[0])
+          const image = `<img class="padding" src="${data.results[0].picture.large}" alt="Ooops..."></img>`
+          const city = `<div class="padding">City: ${data.results[0].location.city}</div>`
+          const country = `<div class="padding">Country: ${data.results[0].location.country}</div>`
+          const name = `<div class="padding">Full Name: ${data.results[0].name.first} ${data.results[0].name.last}</div>`
+          const postcode = `<div class="padding">Postcode: ${data.results[0].location.postcode}</div>`
 
           elements.push(image, city, country, name, postcode)
 
